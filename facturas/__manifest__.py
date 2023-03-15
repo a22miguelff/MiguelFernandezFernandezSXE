cee05bda2cc82254f2afcfccb231e6fe9a5cd1c8
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-03-15 11:29:09 +0100

    Add files via upload

---
+++ facturas/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Facturas',
+    'version': '1.0',
+    'author': 'Miguel Fernández Fernández',
+    'summary': 'Módulo para gestionar facturas',
+    'description': 'Este módulo permite gestionar facturas en Odoo',
+    'category': 'Contabilidad',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/factura_form.xml',
+        'views/factura_calendar.xml',
+        'views/factura_kanban.xml',
+        'views/factura_list.xml',
+        'demo/demo.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'icon': 'static/description/icon.png',
+}
