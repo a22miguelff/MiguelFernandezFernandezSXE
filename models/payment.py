ab0eaa508859a4fb05ffb95118bf98ff3edc92b0
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-03-21 22:25:53 +0100

    Update payment.py

--- models/payment.py
+++ models/payment.py
@@ -4,4 +4,4 @@ class Payment(models.Model):
     name = 'account.payment'
     _inherit = 'account.payment'
     
-    payment_method_id = fields.Many2one('payment.method', string='Payment Method')
+    payment_method_id = fields.Many2one('payment.method', string='Payment Method', required=True)
