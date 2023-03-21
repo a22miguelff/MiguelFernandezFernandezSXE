dd10f490e279568c8d6366b8a8cdfe7385b1fe96
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-03-21 22:23:11 +0100

    Update payment_method.py

--- models/payment_method.py
+++ models/payment_method.py
@@ -1,1 +1,24 @@
+from odoo import models, fields, api
+
+class PaymentMethod(models.Model):
+    _name = 'payment.method'
+    _description = 'Payment Method'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(string='Active', default=True)
+    description = fields.Text(string='Description')
+    payment_type = fields.Selection(
+        [('credit_card', 'Credit Card'), ('debit_card', 'Debit Card'), ('bank_transfer', 'Bank Transfer')],
+        string='Payment Type', required=True)
+
+    @api.model
+    def create(self, vals):
+        res = super(PaymentMethod, self).create(vals)
+        # Your custom code here
+        return res
+
+    def write(self, vals):
+        res = super(PaymentMethod, self).write(vals)
+        # Your custom code here
+        return res
 
