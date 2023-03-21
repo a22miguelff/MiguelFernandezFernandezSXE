952692815c5d117f3e60767db69a5662d840816a
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-03-21 23:49:21 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import invoice, payment, payment_method
--- models/payment.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class Payment(models.Model):
-    _name = 'account.payment'
-    
-    payment_method_id = fields.Many2one('payment.method', string='Payment Method', required=True)
