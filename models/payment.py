1c5e7827dee32e45ce3770c0856885f481d88497
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-03-21 23:19:46 +0100

    Update payment.py

--- models/payment.py
+++ models/payment.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
 
 class Payment(models.Model):
-    name = 'account.payment'
+    _name = 'account.payment'
     
     payment_method_id = fields.Many2one('payment.method', string='Payment Method', required=True)
