647b74f436bb06968c98636cffaa822233be1a7d
a22miguelff <113439412+a22miguelff@users.noreply.github.com>
2023-05-22 10:04:31 +0200

    Update invoice.py

--- models/invoice.py
+++ models/invoice.py
@@ -7,3 +7,7 @@ class Invoice(models.Model):
     recurring_interval = fields.Selection([('daily', 'Daily'), ('weekly', 'Weekly'), ('monthly', 'Monthly'), ('yearly', 'Yearly')], string='Recurring Interval')
     recurring_last_date = fields.Date(string='Last Date of Recurring Invoice')
     payment_method_id = fields.Many2one('payment.method', string='Payment Method', required=True)
+    
+    calendar_event_id = fields.Many2one('calendar.event', string='Calendar Event')
+    kanban_state = fields.Selection([('normal', 'Normal'), ('blocked', 'Blocked'), ('done', 'Done')], string='Kanban State')
+    
